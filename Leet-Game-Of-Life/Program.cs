5ab7fb274025a84340352eec9cf4f19c2606ff77
m-mazur <mazur.marcus@gmail.com>
2015-05-20 09:13:29 +0200

    initial commit

---
+++ Leet-Game-Of-Life/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Game_Of_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
