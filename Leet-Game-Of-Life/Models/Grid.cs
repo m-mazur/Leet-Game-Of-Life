8414e71ee0c20c5fd002ec647a763dab00f6a546
begumdogan <begumdogan0@gmail.com>
2015-05-21 10:32:17 +0200

    added dummy grid

---
+++ Leet-Game-Of-Life/Models/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Game_Of_Life.Models
+{
+    class Grid
+    {
+        private Cell dummyCell;
+
+        public Cell DummyCell
+        {
+            get { return dummyCell; }
+            set { dummyCell = value; }
+        }
+
+
+    }
+}
