c56c0e3a76e9eecc2485bc28c98e3d466db7544a
m-mazur <mazur.marcus@gmail.com>
2015-05-27 13:32:47 +0200

    refactoring

---
+++ Leet-Game-Of-Life.Web/Models/GridProcessor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Leet_Game_Of_Life.Web.Models
+{
+    public class GridProcessor
+    {
+        private Grid grid;
+        private int rowCount;
+        private int columnCount;
+
+        public GridProcessor()
+        {
+            this.grid = new Grid();
+        } 
+
+        public List<Cell> CreateProcessedList(List<Cell> unProcessedGrid)
+        {
+            var processedList = grid.CreateGrid(15, 40);
+
+            foreach (var cell in processedList.Reverse<Cell>())
+            {
+                var newCell = unProcessedGrid.Find(tempCell => (tempCell.X.Equals(cell.X)) && (tempCell.Y.Equals(cell.Y)));
+                var index = processedList.IndexOf(cell);
+
+                if (newCell != null)
+                {
+                    processedList.Remove(cell);
+                    processedList.Insert(index, newCell);
+                }
+            }
+
+            return processedList;
+        }
+
+        public List<Cell> CreateContextGrid(List<Cell> processedGrid, Cell referenceCell)
+        {
+            SetRowAndColumnCount(processedGrid);
+
+            var contextGrid = new List<Cell>();
+
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X - 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y - 1, true))));
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(referenceCell.X) && tempCell.Y.Equals(WrapEdges(referenceCell.Y - 1, true))));
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X + 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y - 1, true))));
+
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X - 1, false)) && tempCell.Y.Equals(referenceCell.Y)));
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X + 1, false)) && tempCell.Y.Equals(referenceCell.Y)));
+
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X - 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y + 1, true))));
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(referenceCell.X) && tempCell.Y.Equals(WrapEdges(referenceCell.Y + 1, true))));
+            contextGrid.Add(processedGrid.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X + 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y + 1, true))));
+
+            contextGrid.RemoveAll(tempCell => tempCell == null);
+
+            return contextGrid;
+        }
+
+        public List<Cell> CreateHoldingGrid(List<Cell> grid) 
+        {
+            return new List<Cell>(grid);
+        }
+
+        private void SetRowAndColumnCount(List<Cell> processedGrid)
+        {
+            rowCount = processedGrid.FindAll(tempCell => tempCell.X.Equals(0)).Count;
+            columnCount = processedGrid.FindAll(tempCell => tempCell.Y.Equals(0)).Count; ;
+        }
+
+        private int WrapEdges(int referenceCellPosition, bool isRow)
+        {
+            var value = isRow ? rowCount : columnCount;
+
+            if (referenceCellPosition < 0)
+            {
+                referenceCellPosition += value;
+            }
+            
+            if (referenceCellPosition > value - 1)
+            {
+                referenceCellPosition -= value;
+            }
+
+            return referenceCellPosition;
+        }
+    }
+}
--- Leet-Game-Of-Life.Web/Models/RulesProcessor.cs
+++ Leet-Game-Of-Life.Web/Models/RulesProcessor.cs
@@ -7,40 +7,33 @@ namespace Leet_Game_Of_Life.Web.Models
 {
     public class RulesProcessor
     {
-        private List<Cell> initialList;
+        private List<Cell> grid;
         private List<Cell> holdingList;
-        private Grid grid;
-        private int neighborCount;
-        private int column;
-        private int row;
+        private GridProcessor gridProcessor;
 
         public RulesProcessor()
         {
-            this.grid = new Grid();
-            this.neighborCount = 0;
+            this.gridProcessor = new GridProcessor();
         }
 
         public List<Cell> CheckNeighborStateAndRunLogic(List<Cell> snapshot)
-        {
-            initialList = ProcessList(snapshot);
-            holdingList = new List<Cell>(initialList);
-            List<List<Cell>> finalList = new List<List<Cell>>();
-            column = FindColumnCount();
-            row = FindRowCount();
+        { 
+            this.grid = gridProcessor.CreateProcessedList(snapshot);
+            this.holdingList = gridProcessor.CreateHoldingGrid(grid);
 
-            CheckRules(initialList);
-
-
-            finalList.Add(holdingList);
+            CheckRules(grid);
 
             return holdingList;
         }
 
         private void CheckRules (List<Cell> list) 
         {
-            foreach (var cell in initialList)
+            var holdingList = gridProcessor.CreateHoldingGrid(list);
+
+            foreach (var cell in grid)
             {
-                var contextGrid = CreateContextGrid(initialList, cell);
+                var contextGrid = gridProcessor.CreateContextGrid(grid, cell);
+                var neighborCount = 0;
 
                 foreach (var cellFromContext in contextGrid)
                 {
@@ -50,99 +43,27 @@ namespace Leet_Game_Of_Life.Web.Models
                     }
                 }
 
-                IfCellHasTwoOrThreeLivingNeighbors(cell);
-                IfCellIsDeadAndHasThreeNeighbors(cell);
-
-                neighborCount = 0;
+                IfCellHasTwoOrThreeLivingNeighbors(cell, neighborCount);
+                IfCellIsDeadAndHasThreeNeighbors(cell, neighborCount);
             }
         }
-        
-        public List<Cell> ProcessList(List<Cell> list)
-        {
-            List<Cell> processedList = grid.CreateGrid(15, 40);
-            
-            foreach (var cell in processedList.Reverse<Cell>())
-            {
-                var newCell = list.Find(tempCell => (tempCell.X.Equals(cell.X)) && (tempCell.Y.Equals(cell.Y)));
-                var index = processedList.IndexOf(cell);
-                
-                if (newCell != null)
-                {
-                    processedList.Remove(cell);
-                    processedList.Insert(index, newCell);
-                }
-            }
 
-            return processedList;
-        }
-
-        private void IfCellHasTwoOrThreeLivingNeighbors(Cell cell)
+        private void IfCellHasTwoOrThreeLivingNeighbors(Cell cell, int neighborCount)
         {
             if (neighborCount < 2 || neighborCount > 3)
             {
-                holdingList.RemoveAt(initialList.IndexOf(cell));
-                holdingList.Insert(initialList.IndexOf(cell), new Cell(cell.X, cell.Y, true));
+                holdingList.RemoveAt(grid.IndexOf(cell));
+                holdingList.Insert(grid.IndexOf(cell), new Cell(cell.X, cell.Y, true));
             }
         }
 
-        private void IfCellIsDeadAndHasThreeNeighbors(Cell cell)
+        private void IfCellIsDeadAndHasThreeNeighbors(Cell cell, int neighborCount)
         {
             if (cell.IsDead && neighborCount == 3)
             {
-                holdingList.RemoveAt(initialList.IndexOf(cell));
-                holdingList.Insert(initialList.IndexOf(cell), new Cell(cell.X, cell.Y, false));
+                holdingList.RemoveAt(grid.IndexOf(cell));
+                holdingList.Insert(grid.IndexOf(cell), new Cell(cell.X, cell.Y, false));
             }
-        }
-
-        private int FindRowCount()
-        {
-            int rowCount = 0;
-            rowCount = initialList.FindAll(tempCell => tempCell.X.Equals(0)).Count;
-            return rowCount;
-        }
-
-        private int FindColumnCount()
-        {
-            int colCount = 0;
-            colCount = initialList.FindAll(tempCell => tempCell.Y.Equals(0)).Count;
-            return colCount;
-        }
-
-        private int WrapEdges(int referenceCellPosition, bool isRow)
-        {
-            int value = 0;
-            value = isRow ? row : column;
-
-            if (referenceCellPosition < 0)
-            {
-                referenceCellPosition += value;
-            }
-            if (referenceCellPosition > value - 1)
-            {
-                referenceCellPosition -= value;
-            }
-
-            return referenceCellPosition;
-        }
-
-        private List<Cell> CreateContextGrid(List<Cell> initialGrid, Cell referenceCell)
-        {
-            var tempList = new List<Cell>();
-
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X - 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y - 1, true))));
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(referenceCell.X) && tempCell.Y.Equals(WrapEdges(referenceCell.Y - 1, true))));
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X + 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y - 1, true))));
-
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X - 1, false)) && tempCell.Y.Equals(referenceCell.Y)));
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X + 1, false)) && tempCell.Y.Equals(referenceCell.Y)));
-
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X - 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y + 1, true))));
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(referenceCell.X) && tempCell.Y.Equals(WrapEdges(referenceCell.Y + 1, true))));
-            tempList.Add(initialList.Find(tempCell => tempCell.X.Equals(WrapEdges(referenceCell.X + 1, false)) && tempCell.Y.Equals(WrapEdges(referenceCell.Y + 1, true))));
-
-            tempList.RemoveAll(tempCell => tempCell == null);
-
-            return tempList;
         }
     }
 }
