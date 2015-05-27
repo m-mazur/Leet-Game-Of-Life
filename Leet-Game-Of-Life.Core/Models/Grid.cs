95fcc423b59937415b1e0674cf7bafea639bfe14
m-mazur <mazur.marcus@gmail.com>
2015-05-27 13:39:19 +0200

    refactoring

--- Leet-Game-Of-Life.Core/Models/Grid.cs
+++ Leet-Game-Of-Life.Core/Models/Grid.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Leet_Game_Of_Life.Core;
 
-namespace Leet_Game_Of_Life.Web.Models
+namespace Leet_Game_Of_Life.Core.Models
 {
     public class Grid
     {
