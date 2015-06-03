890bac88260f77bc5eedd1914ed761acb02ea482
Marcus Mazur <marcus.mazur@tretton37.com>
2015-06-03 10:33:54 +0200

    mobile

--- Leet-Game-Of-Life.Web/Static/js/grid.viewmodel.js
+++ Leet-Game-Of-Life.Web/Static/js/grid.viewmodel.js
@@ -67,7 +67,7 @@ var ViewModel = function (gridService, gridHelpers) {
     };
 
     if (media.matches) {
-        getInitialGrid(10, 12);
+        getInitialGrid(14, 12);
     }else {
         getInitialGrid(15, 25);
     }
